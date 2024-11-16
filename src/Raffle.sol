f9eef1c381024cfa593038f72e6e6a48c2be53d5
Ashein00 <ashen.induwara@hotmail.com>
2024-11-16 15:35:26 +0530

    Project setup completed.

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,4 +1,25 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-contract Raffle {}
+/**
+ * @title Raffle Contract
+ * @author Ashen Thilakarathna
+ * @notice This contract is for creating a simple raffle
+ * @dev Implement Chainlink VRFv2.5
+ */
+contract Raffle {
+    uint256 private immutable i_entranceFee;
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable {}
+
+    function pickWinner() public {}
+
+    /** Getter Functions */
+    function getEntranceFee() external view returns (uint256) {
+        return i_entranceFee;
+    }
+}
