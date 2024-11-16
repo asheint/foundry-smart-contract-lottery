0ee30eaa51781bed9378d7626e3af54560c58e51
Ashein00 <ashen.induwara@hotmail.com>
2024-11-16 22:45:54 +0530

    Block Timestamp added

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -34,16 +34,21 @@ contract Raffle {
     error Raffle__SendMoreToEnterRaffle();
 
     uint256 private immutable i_entranceFee;
+    // @dev The duration of the lottery in seconds
+    uint256 private immutable i_interval;
     address payable[] public s_players;
+    uint256 private s_lastTimeStamp;
 
     /* Events */
     event RaffleEntered(address indexed player);
 
-    constructor(uint256 entranceFee) {
+    constructor(uint256 entranceFee, uint256 interval) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
     }
 
-    function enterRaffle() public payable {
+    function enterRaffle() external payable {
         // require(msg.value >= i_entranceFee, "Not enough Eth sent!");
         // require(msg.value >= i_entranceFee, SendMoreToEnterRaffle());
         if (msg.value < i_entranceFee) {
@@ -55,7 +60,16 @@ contract Raffle {
         emit RaffleEntered(msg.sender);
     }
 
-    function pickWinner() public {}
+    // 1. Get a random number
+    // 2. Use random number to pick a player
+    // 3. Be automatically called
+    function pickWinner() external {
+        // check to see if enough time has passed
+        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
+            revert();
+        }
+        // Get our random number
+    }
 
     /** Getter Functions */
     function getEntranceFee() external view returns (uint256) {
