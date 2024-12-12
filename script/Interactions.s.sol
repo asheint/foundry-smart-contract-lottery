efdd9825df4baef013821fe2f6e4c99f0935eea8
Ashein00 <ashen.induwara@hotmail.com>
2024-12-12 16:19:24 +0530

    Added i nteractions.s.sol

---
+++ script/Interactions.s.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script, console} from "lib/forge-std/src/Script.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
+import {VRFCoordinatorV2_5Mock} from "lib/chainlink-brownie-contracts/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
+
+contract CreateSubscription is Script {
+    function createSubscriptionUsingConfig() public returns (uint256, address) {
+        HelperConfig helperConfig = new HelperConfig();
+        address vrfCoordinator = helperConfig.getConfig().vrfCoordinator;
+        (uint256 subId, ) = createSubscription(vrfCoordinator);
+        return (subId, vrfCoordinator);
+    }
+
+    function createSubscription(
+        address vrfCoordinator
+    ) public returns (uint256, address) {
+        console.log("Creating a subscription on Chain Id: ", block.chainid);
+        vm.startBroadcast();
+        uint256 subId = VRFCoordinatorV2_5Mock(vrfCoordinator)
+            .createSubscription();
+        vm.stopBroadcast();
+
+        console.log("Subscription Id: ", subId);
+        console.log(
+            "Please update the subscription Id in your HelperConfig.s.sol"
+        );
+        return (subId, vrfCoordinator);
+    }
+
+    function run() public {
+        createSubscriptionUsingConfig();
+    }
+}
